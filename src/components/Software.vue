40382d0518ec007d21f101725e3eabbb0cf9cba6
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-10-19 22:07:15 +0200

    init software.vue

---
+++ src/components/Software.vue
@@ -0,0 +1,21 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return { subjects: [{ name: 'UX/UI Design |'},{ name: 'Branding graphic design |'},{ name: 'Website development |'}]}
+    }
+})
+</script>
+
+
+<template>
+    <div>
+        <div>
+           <p>Dev</p> 
+        </div>
+        <div>
+            <span>HTML5</span>
+        </div>
+    </div>
+</template>
