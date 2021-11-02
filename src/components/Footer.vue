cc9de99323edc69997acf3216acd5f8a8815ada4
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-02 12:35:44 +0100

    link footer anim

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,16 @@
     <footer data-scroll-section class="h-32" >
         <div class="flex justify-between">
             <p>edwbr.dev -> 2021</p>
-            <div>
-                <span>Linkedin</span>
-                <span>Github</span>
-                <span>Behance</span>
+            <div class="space-x-10">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/edwbr/">
+                    <span class="trait">Linkedin</span>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/P4tt4te">
+                    <span class="trait">Github</span>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.behance.net/edwbr">
+                    <span class="trait">Behance</span>
+                </a>
             </div>
         </div>
         
