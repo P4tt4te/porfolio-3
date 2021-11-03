385cd089b2638ac9811bb95c091dac430911334e
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-03 15:38:39 +0100

    flag change with lang
    / peut-être changer le drapeau usa /

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,6 @@ export default {
 
   },
   mounted() {
-    console.log(this.$t('test'));
     console.log(this.$i18n.locale);
   },
   methods: {
@@ -52,7 +51,7 @@ export default {
 
 <template>
     <header data-scroll-section class="flex justify-between">
-        <nav class="flex space-x-5">
+        <nav class="flex space-x-6 items-center">
           <svg @click="animmenu()" width="125" height="116" viewBox="0 0 125 116" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M2.54382 25.3086C9.54112 10.5317 28.7438 7.94403 44.581 3.92954C58.3483 0.439747 72.4153 -1.10712 85.5548 4.26658C99.4451 9.94735 111.225 19.9744 117.342 33.6867C123.942 48.4843 127.433 66.262 119.522 80.4148C111.888 94.0699 94.0846 95.6066 79.532 101.323C64.2249 107.336 48.8282 120.787 33.8245 114.078C18.7594 107.341 18.1164 86.9807 12.6284 71.4067C7.19466 55.9867 -4.45745 40.0938 2.54382 25.3086Z" fill="#00D1FF"/>
             <circle id="cercle1" cx="43" cy="44" r="15" fill="white"/>
@@ -74,7 +73,7 @@ export default {
           </div>
         </nav>
         <div class="flex space-x-5">
-            <img @click="changeLanguage($i18n.locale)" src="/src/assets/header/flags/france.svg" alt="">
+            <img @click="changeLanguage($i18n.locale)" :src="this.$t('src')" alt="">
             <img src="/src/assets/header/sunset.svg" alt="">
         </div>
     </header>
