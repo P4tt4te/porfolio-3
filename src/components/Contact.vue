b5ffb9b1871f324bc2ce927f532526a495c4989f
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-03 19:18:01 +0100

    init form 4

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,13 @@
 <template>
     <div data-scroll-section :id="'Contact'">
         <h3 data-scroll data-scroll-speed="3" class="text-8xl font-extrabold">Contact.</h3>
-        <form name="contact" netlify>
+        <form name="contact" method="POST" data-netlify="true">
             <div>
                 <label for="email">Votre email</label>
                 <input type="email" name="email" id="email" required>
             </div>
             <div>
-                <label for="message">Votre message</label>
-                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                <label>Message: <textarea name="message"></textarea></label>
             </div>
             <div>
                 <button type="submit">envoyer</button>
