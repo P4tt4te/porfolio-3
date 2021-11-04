93f2670b06b2cd8abc8cc48cd69377846cf5c6d4
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-04 11:54:36 +0100

    test form 9

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,9 +1,32 @@
 <script>
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
     data() {
         return { email: "",message: "" }
+    },
+    methods: {
+        encode (data) {
+            return Object.keys(data)
+                .map(
+                    key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+                )
+                .join("&");
+        },
+        envoi() {
+            const axiosConfig = {
+                header: { "Content-Type": "application/x-www-form-urlencoded" }
+        };
+        axios.post(
+            "/",
+            this.encode({
+            "form-name": "ask-question",
+            ...this.form
+            }),
+            axiosConfig
+        );
+        }
     }
 })
 </script>
@@ -11,7 +34,7 @@ export default defineComponent({
 <template>
     <div data-scroll-section :id="'Contact'" class="space-y-10">
         <h3 data-scroll data-scroll-speed="1" class="text-8xl font-extrabold">Contact.</h3>
-        <form class="space-y-10 p-3 flex flex-col items-center border border-blue-light" name="contact" method="post" data-netlify="true">
+        <form @submit.prevent="envoi()" class="space-y-10 p-3 flex flex-col items-center border border-blue-light" name="contact">
             <div class="space-x-5">
                 <label for="votre_email">Votre email</label>
                 <input class="bg-blue-dark p-1 text-white" type="email" v-model="email" required>
