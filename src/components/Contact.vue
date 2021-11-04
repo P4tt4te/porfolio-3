2ca7a624eb99c1df884f7706626e9a1c1a535728
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-04 11:14:34 +0100

    test form 6

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,13 +1,13 @@
 <template>
     <div data-scroll-section :id="'Contact'" class="space-y-10">
         <h3 data-scroll data-scroll-speed="1" class="text-8xl font-extrabold">Contact.</h3>
-        <form class="space-y-10 p-3 flex flex-col items-center border border-blue-light" name="contact" method="POST" netlify>
+        <form class="space-y-10 p-3 flex flex-col items-center border border-blue-light" netlify>
             <div class="space-x-5">
-                <label for="email">Votre email</label>
-                <input class="bg-blue-dark p-1 text-white" type="email" name="email" id="email" required>
+                <label for="votre_email">Votre email</label>
+                <input class="bg-blue-dark p-1 text-white" type="email" name="email" id="votre_email" required>
             </div>
             <div>
-                <label >Message: <textarea class=" ml-5 bg-blue-dark p-1 text-white" name="message"></textarea></label>
+                <label for="message_envoi" >Message: <textarea class=" ml-5 bg-blue-dark p-1 text-white" id="message_envoi" name="message"></textarea></label>
             </div>
             <div>
                 <button class="text-white bg-blue-dark p-2 rounded" type="submit">envoyer</button>
