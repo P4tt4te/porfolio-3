24cb4554a416900af425cabb572529f370eb6236
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-05 15:28:14 +0100

    test form

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Vuei18n from '@intlify/vite-plugin-vue-i18n'
+import PrerenderSPAPlugin from 'prerender-spa-plugin'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),Vuei18n({
     include: path.resolve(__dirname, './src/locales/**'),
+  }),PrerenderSPAPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: ['/']
   })]
 })
