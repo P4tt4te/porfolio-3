e03ede5e52355649e9059eda7794259b558ddaea
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-11-05 14:28:06 +0100

    form test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -41,10 +41,10 @@ export default defineComponent({
         <form @submit.prevent="envoi()" class="space-y-10 p-3 flex flex-col items-center border border-blue-light" name="contact">
             <div class="space-x-5">
                 <label for="votre_email">Votre email</label>
-                <input class="bg-blue-dark p-1 text-white" type="email" @input="ev => form.email = ev.target.value" required>
+                <input class="bg-blue-dark p-1 text-white" type="email" @input="ev => form.email = ev.target.value" required="required">
             </div>
             <div>
-                <label for="message_envoi" >Message: <textarea class=" ml-5 bg-blue-dark p-1 text-white"  @input="ev => form.message = ev.target.value" name="message"></textarea></label>
+                <label for="message_envoi" >Message: <textarea class=" ml-5 bg-blue-dark p-1 text-white" required="required"  @input="ev => form.message = ev.target.value" name="message"></textarea></label>
             </div>
             <div>
                 <button class="text-white bg-blue-dark p-2 rounded" type="submit">envoyer</button>
