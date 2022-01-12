075472ad2be1f10b5eb9246bb63b5efc98417b91
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2022-01-12 11:58:07 +0100

    opti final

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,7 +2,7 @@
     <div data-scroll-section class="">
         <div data-scroll data-scroll-speed="3" class="flex relative justify-center">
             <h1 id="nom" class="barre font-Sunydale text-6xl md:text-4xl lg:text-6xl xl:text-8xl text-indigo-800 z-2 px-20 py-28 sm:py-48 md:py-56 absolute">Edward Brunetiere</h1>
-            <img class="" src="../assets/title/montagne.png" alt="">
+            <img class="" src="../assets/title/montagne.webp" alt="">
             
         </div>
         <h2 data-scroll data-scroll-speed="2" class="text-center text-xl lg:text-2xl">{{ $t('title.desc') }}</h2>
