0a11f2b4176d575ca7f79ffdf1b98d3c835f3aec
Edward B <57622377+P4tt4te@users.noreply.github.com>
2022-06-06 11:11:50 +0200

    Update Cv + and Textcarousel

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,33 @@
 <template>
-    <footer data-scroll-section class="barre h-32" >
-        <div class="flex flex-col space-y-10 items-center sm:items-start sm:space-y-0 sm:flex-row sm:justify-between">
-            <p>edwbr.dev -> 2021</p>
-            <div class="space-x-10">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/edwbr/">
-                    <span class="trait">Linkedin</span>
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/P4tt4te">
-                    <span class="trait">Github</span>
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.behance.net/edwbr">
-                    <span class="trait">Behance</span>
-                </a>
-            </div>
-        </div>
-        
-    </footer>
+  <footer data-scroll-section class="barre h-36">
+    <div
+      class="flex flex-col space-y-10 items-center sm:items-start sm:space-y-0 sm:flex-row sm:justify-between"
+    >
+      <p>edwbr.dev -> 2021</p>
+      <div class="space-x-10">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/edwbr/"
+        >
+          <span class="trait">Linkedin</span>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/P4tt4te"
+        >
+          <span class="trait">Github</span>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.behance.net/edwbr"
+        >
+          <span class="trait">Behance</span>
+        </a>
+      </div>
+    </div>
+    <div id="wcb" class="carbonbadge py-5"></div>
+  </footer>
 </template>
--- src/components/Textcarousel.vue
+++ src/components/Textcarousel.vue
@@ -1,40 +1,68 @@
 <script>
-import { defineComponent } from 'vue';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import SwiperCore, {
-  Autoplay
-} from 'swiper';
-import 'swiper/css';
-
+import { defineComponent } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 // install Swiper modules
-SwiperCore.use([Autoplay]);
+SwiperCore.use([Autoplay, Pagination]);
 
 export default defineComponent({
-    components: {
-        Swiper,
-        SwiperSlide,
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    onSwiper(swiper) {
+      console.log(swiper);
     },
-    methods: {
-      onSwiper(swiper) {
-        console.log(swiper)
-      },
-      onSlideChange() {
-        console.log('slide change')
-      },
+    onSlideChange() {
+      console.log("slide change");
     },
-    data() {
-        return { subjects: [{ name: 'UX/UI Design'},{ name: 'Branding'},{ name: 'Website development'},{ name: 'Logo design'}]}
-    }
-})
+  },
+  data() {
+    return {
+      subjects: [
+        { name: "UX/UI Design" },
+        { name: "Branding" },
+        { name: "Website development" },
+        { name: "Logo design" },
+      ],
+    };
+  },
+});
 </script>
 
 <template>
-    <div id="direction" data-scroll-section>
-        <h3 data-scroll data-scroll-speed="3" class="text-5xl sm:text-6xl xl:text-8xl font-extrabold">{{ $t('textcarousel.title') }}</h3>
-        <swiper class="text-sm sm:text-md md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-7xl text-grey" :slides-per-view="3" :autoplay='{ "delay": 1500, "disableOnInteraction": false }' :centeredSlides="true" :space-between="50" :loop="true" @swiper="onSwiper" @slideChange="onSlideChange">
-            <swiper-slide class="text-center" v-for="subject in subjects" :key="subject">{{ subject.name }}</swiper-slide>
-        </swiper>
-        
-    </div>
+  <div id="direction" data-scroll-section>
+    <h3
+      data-scroll
+      data-scroll-speed="3"
+      class="text-4xl sm:text-6xl xl:text-8xl font-extrabold mb-5"
+    >
+      {{ $t("textcarousel.title") }}
+    </h3>
+    <swiper
+      class="text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-7xl text-grey h-44"
+      :pagination="{
+        clickable: true,
+      }"
+      :slidesPerView="'auto'"
+      :autoplay="{ delay: 1500, disableOnInteraction: false }"
+      :centeredSlides="true"
+      :space-between="50"
+      :loop="true"
+      :modules="modules"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide
+        class="text-center items-center"
+        v-for="subject in subjects"
+        :key="subject"
+        >{{ subject.name }}</swiper-slide
+      >
+    </swiper>
+  </div>
 </template>
